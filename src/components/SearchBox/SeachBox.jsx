c5242d688de31edbf1217c56c93c2243834d1eb2
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-07-15 19:37:10 +0300

    search_box

---
+++ src/components/SearchBox/SeachBox.jsx
@@ -0,0 +1,19 @@
+import s fro "./SeachBox.module.css";
+
+const SearchBox = ({ filter, onFilter }) => {
+  return (
+    <div>
+      <span className={s.wrapper}>
+        <p>Find contacts by name</p>
+        <input
+          type="text"
+          value={filter}
+          onChange={(e) => onFilter(e.target.value)}
+          className={s.searchInput}
+        ></input>
+      </span>
+    </div>
+  );
+};
+
+export default SearchBox;
