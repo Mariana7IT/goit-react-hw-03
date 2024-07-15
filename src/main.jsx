a6e6ef45e1feceb1fc03da7787ce4efa31f2b52a
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-07-15 18:14:39 +0300

    module.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import "modern-normalize";
-import App from './components/App.jsx';
-import '/src/index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App.jsx"; 
+import "./index.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-<>
-<App />
-</>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
